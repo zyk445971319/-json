e69f1804e87c5e52c43f4f43be958a122fae9cca
zyk445971319 <33056310+zyk445971319@users.noreply.github.com>
2022-04-06 01:01:16 +0800

    Update baidu.lua

--- baidu.lua
+++ baidu.lua
@@ -45,7 +45,7 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
-                    'Host: ' .. host .. ':' .. port .. '\r\n' ..
+                    'Host: tms.dingtalk.com:443\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n' ..
                     'User-Agent: baiduboxapp\r\n\r\n'
